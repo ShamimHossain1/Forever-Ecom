10ea0f279330cd96dd09265bc31cf8ae12792552
ShamimHossain1 <shamimweb78@gmail.com>
2026-01-02 12:23:20 -0500

    about and contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,66 @@
-import React from 'react';
-
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
 const About = () => {
-    return (
-        <div>
-            
-            
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t border-gray-200">
+        <Title text1={"About"} text2={"Us"}></Title>
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+          <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            aspernatur natus, consectetur accusamus facilis ab magni quos rerum
+            minus repellat minima et animi ut commodi vero alias voluptates
+            sequi sed quibusdam soluta magnam autem! Atque aliquam porro
+            possimus sint voluptates quos earum consequuntur commodi pariatur
+            laborum, perferendis, natus hic omnis!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta atque
+            in porro aliquam sint nulla ducimus alias hic rerum nam iste
+            pariatur ex sunt, ipsum nostrum exercitationem itaque quia laborum.
+            Nam amet alias autem illum architecto quibusdam, vero officia quia.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus,
+            totam! Adipisci nostrum quod ratione numquam tempore quisquam,
+            mollitia dignissimos expedita? Iusto dolores nemo, doloribus
+            provident quos magni harum minus sequi expedita odio corrupti cumque
+            ullam. Nisi aut possimus laborum id fugiat. Perferendis amet quidem
+            iste quae laborum cumque facere labore.
+          </p>
         </div>
-    );
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={"WHY"} text2={"CHOOSE US"}></Title>
+
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-10 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sit eius recusandae doloribus laudantium voluptate.</p>
+        </div>
+        <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-10 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sit eius recusandae doloribus laudantium voluptate.</p>
+        </div>
+        <div className='border border-gray-200 px-10 md:px-16 py-8 sm:py-10 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sit eius recusandae doloribus laudantium voluptate.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Title from '../components/Title';
+import { assets } from '../assets/frontend_assets/assets';
+import NewsLetterBox from '../components/NewsLetterBox';
 
 const Contact = () => {
     return (
         <div>
-            
+            <div className='text-center border-gray-200 text-2xl pt-10 border-t'>
+            <Title text1={'Contact'} text2={'Us'}></Title>
+            </div>
+
+            <div className='mt-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+                <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+                <div className='flex flex-col justify-center items-start gap-6'>
+                    <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+                    <p className='text-gray-500'>53 Miramar Cres <br />Scarborough, ON M1M 1M1</p>
+                    <p className='text-gray-500'>tel: 416-555-5555 <br />Email: 9FtFt@example.com</p>
+                    <p className='font-semibold text-gray-600 text-xl'>Careers at Forever</p>
+                    <p className='text-gray-500'> Learn more about our teams and job opportunities</p>
+                    <button className='border border-black hover:bg-black hover:text-white px-6 py-2 transition-all duration-500'>Explore Careers</button>
+                </div>
+            </div>
+            <NewsLetterBox />
         </div>
     );
 };
