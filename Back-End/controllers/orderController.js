cadfbfff28b9ba7a62dcb9625e2bf7b4940b0f8f
ShamimHossain1 <shamimweb78@gmail.com>
2026-01-07 00:55:04 -0500

    `Added new route for order API`

---
+++ Back-End/controllers/orderController.js
@@ -0,0 +1,51 @@
+
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel";
+
+//place orders using cod method
+const placeOrder = async (req, res) => {
+    try {
+        const { userId, items, amount, address } = req.body;
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod:"COD",
+            payment:false,
+            date:Date.now(),
+        }
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        await userModel.findByIdAndUpdate(userId, { cartData: {} });
+
+        res.json({ success: true, message: "Order placed successfully" });
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+// Placing order using stripe method
+
+const placeOrderStripe = async (req, res) => {};
+
+// placing orders using razorpay method
+
+const placeOrderRazorpay = async (req, res) => {};
+
+// All orders data for admin panel
+
+const allOrders = async (req, res) => {};
+
+// User order data for frontend
+
+const userOrders = async (req, res) => {};
+
+// update order status from admin panel
+
+const updateOrderStatus = async (req, res) => {};
+
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateOrderStatus };
--- Back-End/server.js
+++ Back-End/server.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
 app.get('/', (req, res)=>{
     res.send("Api is working");
