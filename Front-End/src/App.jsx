019f72f1696d73aece4ab1013da04c001280e499
ShamimHossain1 <shamimweb78@gmail.com>
2026-01-13 02:11:17 -0500

    `Added .env to .gitignore and updated import statements in App.jsx`

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -11,31 +11,28 @@ import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import SearchBar from "./components/searchBar";
- import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import SearchBar from "./components/SearchBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Verify from "./pages/Verify";
-
-
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <ToastContainer position="top-center"  />
+      <ToastContainer position="top-center" />
       <Navbar />
-  <SearchBar></SearchBar>
+      <SearchBar></SearchBar>
       <Routes>
-        <Route path="/"element={<Home></Home>} />
+        <Route path="/" element={<Home></Home>} />
         <Route path="/collection" element={<Collection />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/contact" element={<Contact />} />
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/place-order" element={<PlaceOrder/>} /> 
-        <Route path="/orders" element={<Orders/>} />
-        <Route path="/verify" element={<Verify/>} />
-
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/verify" element={<Verify />} />
       </Routes>
       <Footer />
     </div>
